4cab7d959fc1671572447f24bf11650587bca6ef
Mary Soarez <mary.soarez@gmail.com>
2023-02-22 13:43:12 -0300

    Projeto de renda percapta para estudos

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+
+    var nomecompleto = "Mary"
+    val texto:String
+    val numero:UInt
+    val pib:ULong = 1869000000000u
+    val populacao:ULong = 211000000u
+    val renda = pib/populacao
+    println("Olá $nomecompleto, A renda Percapta é $renda")
+    println("Olá $nomecompleto, A renda Percapta feita de outra forma é ${pib/populacao}")
+
+}
